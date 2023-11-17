6b8d4a79eed41b533dbcd1534ed3bc6eab0eb1d6
alainhortiz <alainhortiz@gmail.com>
2023-11-17 10:16:28 -0500

    Flujo de control en componentes - @for

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -10,5 +10,6 @@ import { CommonModule } from '@angular/common';
 })
 export class UserComponent {
   username: string = 'Alain';
-  doUserExits: boolean = true;
+  doUserExits: boolean = true; 
+  users = [{id: 0, name: 'Sarah'}, {id: 1, name: 'Amy'}, {id: 2, name: 'Rachel'}, {id: 3, name: 'Jessica'}, {id: 4, name: 'Poornima'}]
 }
