e91dd29805c120631b4bd06b1f2d828eb9cc9a0c
alainhortiz <alainhortiz@gmail.com>
2023-11-18 23:11:59 -0500

    Enlace de propiedades

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -12,4 +12,5 @@ export class UserComponent {
   username: string = 'Alain';
   doUserExits: boolean = true; 
   users = [{id: 0, name: 'Sarah'}, {id: 1, name: 'Amy'}, {id: 2, name: 'Rachel'}, {id: 3, name: 'Jessica'}, {id: 4, name: 'Poornima'}]
+  isEditable: boolean = true;
 }
