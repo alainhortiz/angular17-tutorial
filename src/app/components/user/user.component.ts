3022a6acfea072ddc6e352671c33eb5435b48b88
alainhortiz <alainhortiz@gmail.com>
2023-11-21 16:47:54 -0500

    Control de eventos

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -10,7 +10,14 @@ import { CommonModule } from '@angular/common';
 })
 export class UserComponent {
   username: string = 'Alain';
+
   doUserExits: boolean = true; 
+
   users = [{id: 0, name: 'Sarah'}, {id: 1, name: 'Amy'}, {id: 2, name: 'Rachel'}, {id: 3, name: 'Jessica'}, {id: 4, name: 'Poornima'}]
+  
   isEditable: boolean = true;
+
+  onMouseOver(osName: string) {
+    console.log(osName);  
+    }
 }
